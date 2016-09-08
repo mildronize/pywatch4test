5ef5a0ff6f15bfe5ab015ede09aca22e875ed1ef
Thada Wangthammang <mildronize@gmail.com>
2016-09-08 13:09:46 +0700

    Move code from https://github.com/mildronize/autotest

--- pywatch4test/cli.py
+++ pywatch4test/cli.py
@@ -1,14 +1,52 @@
 # -*- coding: utf-8 -*-
 
 import click
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+from pywatch4test.pywatch4test import PathProcessor
+
+
+class ModifiedHandler(PatternMatchingEventHandler):
+    patterns = ["*.py"]
+
+    def __init__(self, path_processor):
+        super().__init__()
+        self.path_processor = path_processor
+
+    def on_created(self, event):
+        self.path_processor.when_file_changed(event.src_path)
+
+    #  def on_any_event(self, event):
+        #  pass
+
+    def on_modified(self, event):
+        self.path_processor.when_file_changed(event.src_path)
+
 
 @click.command()
 def main(args=None):
     """Console script for pywatch4test"""
-    click.echo("Replace this message by putting your code into "
-                "pywatch4test.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    unittest_dir = "tests"
+    package_name = "simple_calculator"
+    startswith_test = True
 
+    click.echo("Running... pywath4test")
+
+    event_handler = ModifiedHandler(
+            PathProcessor(unittest_dir, package_name, startswith_test))
+    observer = Observer()
+    observer.schedule(event_handler, '.', recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 if __name__ == "__main__":
     main()
--- tests/test_pywatch4test.py
+++ tests/test_pywatch4test.py
@@ -1,44 +1,127 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-test_pywatch4test
-----------------------------------
-
-Tests for `pywatch4test` module.
-"""
-
-
-import sys
 import unittest
-from contextlib import contextmanager
-from click.testing import CliRunner
 
-from pywatch4test import pywatch4test
-from pywatch4test import cli
+from pywatch4test.pywatch4test import PathProcessor
 
 
-
-class TestPywatch4test(unittest.TestCase):
+class TestProcessFilenameAndPackageInvert(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.pp = PathProcessor("tests", "simple_calculator", True)
 
-    def tearDown(self):
-        pass
+    def test_process_filename_and_package_invert_windows_1(self):
+        filename = ".\\tests\\test_calculator.py"
+        self.assertEqual({
+                "test_file_path": "tests\\test_calculator.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package_invert(filename, 'nt'))
 
-    def test_000_something(self):
-        pass
+    def test_process_filename_and_package_invert_windows_2(self):
+        filename = ".\\tests\\sub\\test_calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\sub\\test_calculator.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package_invert(filename, 'nt'))
 
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'pywatch4test.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    def test_process_filename_and_package_invert_windows_3(self):
+        filename = ".\\tests\\calculator_test.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\calculator_test.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package_invert(filename, 'nt', False))
 
+    def test_process_filename_and_package_invert_windows_4(self):
+        filename = ".\\tests\\sub\\calculator_test.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\sub\\calculator_test.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package_invert(filename, 'nt', False))
 
-if __name__ == '__main__':
-    sys.exit(unittest.main())
+    def test_process_filename_and_package_invert_unix_1(self):
+        filename = "./tests/test_calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests/test_calculator.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package_invert(filename))
+
+    def test_process_filename_and_package_invert_unix_2(self):
+        filename = "./tests/sub/test_calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests/sub/test_calculator.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package_invert(filename))
+
+    def test_process_filename_and_package_invert_unix_3(self):
+        filename = "./tests/calculator_test.py"
+        self.assertEqual( {
+                "test_file_path": "tests/calculator_test.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package_invert(filename, startswith_test=False))
+
+    def test_process_filename_and_package_invert_unix_4(self):
+        filename = "./tests/sub/calculator_test.py"
+        self.assertEqual( {
+                "test_file_path": "tests/sub/calculator_test.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package_invert(filename, startswith_test=False))
+
+class TestProcessFilenameAndPackage(unittest.TestCase):
+
+    def setUp(self):
+        self.pp = PathProcessor("tests", "simple_calculator", True)
+
+    def test_process_filename_and_package_windows_1(self):
+        filename = ".\\simple_calculator\\calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\test_calculator.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package(filename, 'nt'))
+
+    def test_process_filename_and_package_windows_2(self):
+        filename = ".\\simple_calculator\\sub\\calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\sub\\test_calculator.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package(filename, 'nt'))
+
+    def test_process_filename_and_package_windows_3(self):
+        filename = ".\\simple_calculator\\calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\calculator_test.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package(filename, 'nt', False))
+
+    def test_process_filename_and_package_windows_4(self):
+        filename = ".\\simple_calculator\\sub\\calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests\\sub\\calculator_test.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package(filename, 'nt', False))
+
+    def test_process_filename_and_package_unix_1(self):
+        filename = "./simple_calculator/calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests/test_calculator.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package(filename))
+
+    def test_process_filename_and_package_unix_2(self):
+        filename = "./simple_calculator/sub/calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests/sub/test_calculator.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package(filename))
+
+    def test_process_filename_and_package_unix_3(self):
+        filename = "./simple_calculator/calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests/calculator_test.py",
+                "package": "simple_calculator.calculator"
+            }, self.pp.process_filename_and_package(filename, startswith_test=False))
+
+    def test_process_filename_and_package_unix_4(self):
+        filename = "./simple_calculator/sub/calculator.py"
+        self.assertEqual( {
+                "test_file_path": "tests/sub/calculator_test.py",
+                "package": "simple_calculator.sub.calculator"
+            }, self.pp.process_filename_and_package(filename, startswith_test=False))
+
